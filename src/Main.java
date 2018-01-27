3d9de7df62532b728a1a0d1beb597ff4569b0fd4
Zach <zevans2@missouriwestern.edu>
2018-01-26 18:56:28 -0600

    Finished Project. Still need to format console output to assignment standards but all functions and test functions are working properly

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    public static Pattern regex = Pattern.compile("[^A-Za-z0-9]");
-    public static Matcher matcher;
-    public static char[] spliterator;
-
+    //Accessible Variables
+    private static Pattern regex = Pattern.compile("[^A-Za-z0-9]");
+    private static char[] spliterator;
 
     public static void main(String[] args) {
         String password;
@@ -18,49 +17,44 @@ public class Main {
         System.out.println("Enter New Password: ");
         password = input.nextLine();
         entropyScore = calculateEntropy(password);
-
-        //Debugging todo: remove before submission
-        System.out.println("Password is: " + password);
-        System.out.println("Entropy is: " + entropyScore);
-        evaluateEntropy(entropyScore);
-        System.out.println();
-
-        int specialCharacterCount = countSpecialCharacters(password);
+        System.out.println();//skip line
 
         //double for log2 equation
-        double number = 20.0;
+        double number = 40.0;
 
         //Could print this from method....
-        System.out.println("Special characters:\t" + specialCharacterCount);
+        System.out.println("Password: \t\t\t" + password);
+        System.out.println("Entropy:\t\t\t" + entropyScore);
+        System.out.println("Password Strength:\t" + evaluateEntropy(entropyScore));
+        System.out.println("Special characters:\t" + countSpecialCharacters(password));
         System.out.println("Has Digit: \t\t\t" + hasDigit(password));
         System.out.println("Has Uppercase: \t\t" + hasUpperCase(password));
         System.out.println("Has Lowercase: \t\t" + hasLowerCase(password));
-        System.out.println("Trimmed Length: \t " + trimmedLength(password));
+        System.out.println("Trimmed Length: \t" + trimmedLength(password));
         System.out.println("Truncated Password:\t" + truncate(password, 20));
-        System.out.println("Log2 of " + number + ":\t\t" + log2(number));
+        System.out.printf("Log2 of " + number + ":\t\t%.2f", log2(number));
+        System.out.println();//skip line
 
     }//end main
-
 
     //Determine the number of types of special characters in the string
     public static int countSpecialCharacters(String s) {
         spliterator = s.toCharArray();
         ArrayList<Character> temp = new ArrayList<>();
         int ret = 0;
-
         //Remove repeated characters
-        for (int i = 0; i<= spliterator.length - 1; i++){
-            if(temp.contains(spliterator[i]))
+        for (int i = 0; i <= spliterator.length - 1; i++) {
+            if (temp.contains(spliterator[i]))
                 spliterator[i] = '0';
             else temp.add(spliterator[i]);
         }
         //Scan password against regex pattern to determine number of special characters
         for (int i = 0; i <= spliterator.length - 1; i++) {
-            matcher = regex.matcher(String.valueOf(spliterator[i]));
+            Matcher matcher = regex.matcher(String.valueOf(spliterator[i]));
             if (matcher.find())
                 ret++;
         }//end for
-        return ret;//todo, checks for the number of special characters used in password
+        return ret;
     }//end countSpecialCharacters
 
     public static boolean hasDigit(String s) {
@@ -68,21 +62,21 @@ public class Main {
         //Check password for digits
         if (s.matches(".*\\d+.*"))
             ret = true;
-        return ret;//todo, checks if password contains a numeric value
+        return ret;
     }//end hasDigit
 
     public static boolean hasUpperCase(String s) {
         boolean ret = false;
         if (!s.equals(s.toLowerCase()))
             ret = true;
-        return ret;//todo, checks if password contains an uppercase character
+        return ret;
     }//end hasUpperCase
 
     public static boolean hasLowerCase(String s) {
         boolean ret = false;
-        if(!s.equals(s.toUpperCase()))
+        if (!s.equals(s.toUpperCase()))
             ret = true;
-        return ret;//todo, checks if password contains a lowercase character
+        return ret;
     }//end hasLowerCase
 
     public static int trimmedLength(String s) {
@@ -95,54 +89,42 @@ public class Main {
 
     public static int calculateRange(String s) {
         int ret = 0;
-
-        if(hasUpperCase(s))
-            ret+=26;
-        if(hasLowerCase(s))
-            ret+=26;
-        if(hasDigit(s))
+        if (hasUpperCase(s)) //add 26 for lowercase letters
+            ret += 26;
+        if (hasLowerCase(s)) //add 26 for uppercase letters
+            ret += 26;
+        if (hasDigit(s))     //add 10 for containing digits
             ret += 10;
-        ret+=countSpecialCharacters(s);
-
-
-        return ret;//todo, Calculates the range of a string
-        /*Range starts at 0;
-          Add 26 if the string contains an uppercase letter
-          Add 26 if the string contains a lowercase letter
-          Add 10 if the string contains a digit
-          Add the total number of special characters used.
-        */
+        ret += countSpecialCharacters(s); //add 1 for each type of special character used
+        return ret;
     }//end calculateRange
 
     public static String truncate(String s, int n) {
         spliterator = s.toCharArray();
-        String ret = "";
+        StringBuilder ret = new StringBuilder();
         if (s.length() <= n)
-            ret = s;
+            ret = new StringBuilder(s);
         else {
             //loop through and add each character until you have reached n length.
-            for (int i = 0; i <= n-1; i++) {
-                ret = (ret + String.valueOf(spliterator[i]));
+            for (int i = 0; i <= n - 1; i++) {
+                ret.append(String.valueOf(spliterator[i]));
             }//end for
-        //Append ellipses
-        ret = (ret + "...");
+            //Append ellipses
+            ret.append("...");
         }
 
-        return ret;//todo, return the first n characters of password appended by (...).
+        return ret.toString();
         //If n > password.length return password.
     }//end truncate
+
     //Overloaded to allow for truncating numbers
-    public static int truncate(double n){
-        int ret = ((int) n);
-        return ret;
+    private static int truncate(double n) {
+        return ((int) n);
     }
 
     //returns log of x in base 2
     public static double log2(double x) {
-        double ret = Math.log(x)/Math.log(2);
-        ret = Math.round((ret * 10000.0000)/10000.0000);
-
-        return ret;
+        return Math.log(x) / Math.log(2);
     }//end log2
 
     public static int calculateEntropy(String s) {
@@ -152,42 +134,27 @@ public class Main {
         double length;
 
         length = s.length();
-
-        maxEntropy = log2(Math.pow(range,(length - 1)));
-
-        int ret = truncate(maxEntropy);
-
-        return ret;
-
-
-/*
-where
-E is an integer representing the maximum entropy. (Truncate the decimal part; don't round)
-R is the range of the characters.
-    range is 26 if only lower case letters are used
-    range is 52 if upper and lower case letters are used
-    range is 62 if upper, lower, and digits are used.
-    range is 67 if upper, lower, digits, and 5 special characters are used.
-     range is 72 if upper, lower, digits, and 10 special characters are used.
-*/
+        if (length == 0) {//if password is blank
+            return 0;
+        }
+        maxEntropy = log2(Math.pow(range, (length - 1)));
+        return truncate(maxEntropy);
     }//end calculateEntropy
 
-    public static void evaluateEntropy(double entropy) {
+    public static String evaluateEntropy(double entropy) {
         String ret = "Test_Value";
         //Evaluate Entropy
-        if(entropy <= 64)
+        if (entropy <= 64)
             ret = "Very Weak";
-        else if(entropy >64 && entropy <= 80)
+        else if (entropy > 64 && entropy <= 80)
             ret = "Weak";
-        else if(entropy >80 && entropy <= 112)
+        else if (entropy > 80 && entropy <= 112)
             ret = "Moderate";
-        else if(entropy >112 && entropy <= 128)
+        else if (entropy > 112 && entropy <= 128)
             ret = "Strong";
-        else if(entropy >128)
+        else if (entropy > 128)
             ret = "Very Strong";
-        //Print Evaluation
-        System.out.println(ret);
-
+        return ret;
         /*
         Entropy Strength Scale
         -----------------------
@@ -197,9 +164,7 @@ R is the range of the characters.
         113 -  128  Strong
         128 -       Very Strong
          */
-
-    }
-
+    }//end evaluateEntropy
 }//end application
 
 
--- src/MainTest.java
+++ src/MainTest.java
@@ -8,49 +8,89 @@ class MainTest {
 
     @org.junit.jupiter.api.Test
     void countSpecialCharacters() {
-    String test = "test123ABC!!@#$";
-    int expected = 4;
-
-
+        String test = "test123ABC!!@#$";
+        int expected = 4;
+        assertEquals(6, Main.countSpecialCharacters("TestPassword!!-2@#$%"));
+        assertEquals(4, Main.countSpecialCharacters("abc123!!@#$"));
+        assertEquals(2, Main.countSpecialCharacters("  ABC."));
+        assertEquals(0, Main.countSpecialCharacters("ABCdef"));
+        assertEquals(1, Main.countSpecialCharacters("password!"));
     }
 
     @org.junit.jupiter.api.Test
     void hasDigit() {
+        assertEquals(true, Main.hasDigit("password123!"));
+        assertEquals(true, Main.hasDigit("1"));
+        assertEquals(false, Main.hasDigit("password!!"));
+        assertEquals(false, Main.hasDigit("Password!!"));
     }
 
     @org.junit.jupiter.api.Test
     void hasUpperCase() {
+        assertEquals(true, Main.hasUpperCase("Password123!"));
+        assertEquals(true, Main.hasUpperCase("AAA"));
+        assertEquals(false, Main.hasUpperCase("password123!"));
+        assertEquals(false, Main.hasUpperCase("aaa"));
+
     }
 
     @org.junit.jupiter.api.Test
     void hasLowerCase() {
+        assertEquals(true, Main.hasLowerCase("Password123!"));
+        assertEquals(true, Main.hasLowerCase("aaa"));
+        assertEquals(false, Main.hasLowerCase("PASSWORD123!"));
+        assertEquals(false, Main.hasLowerCase("AAA"));
     }
 
     @org.junit.jupiter.api.Test
     void trimmedLength() {
+        assertEquals(8, Main.trimmedLength("   password"));
+        assertEquals(8, Main.trimmedLength("   password   "));
     }
 
     @org.junit.jupiter.api.Test
     void calculateRange() {
+        assertEquals(26, Main.calculateRange("testpassword"));
+        assertEquals(52, Main.calculateRange("Testpassword"));
+        assertEquals(62, Main.calculateRange("TestPassword123"));
+        assertEquals(63, Main.calculateRange("TestPassword123!!"));
+        assertEquals(72, Main.calculateRange("TestPassword123!!@#$%^&*()"));
+
     }
 
     @org.junit.jupiter.api.Test
     void truncate() {
+        assertEquals("TestPass...", Main.truncate("TestPassword!!", 8));
+        assertEquals("TestPassword!!", Main.truncate("TestPassword!!", 20));
     }
 
-    @org.junit.jupiter.api.Test
-    void truncate1() {
-    }
 
     @org.junit.jupiter.api.Test
     void log2() {
+        assertEquals(4.321928094887363, Main.log2(20));
     }
 
     @org.junit.jupiter.api.Test
     void calculateEntropy() {
+        assertEquals(0, Main.calculateEntropy(""));
+        assertEquals(14, Main.calculateEntropy("test"));
+        assertEquals(62, Main.calculateEntropy("TestPassword"));
+        assertEquals(95, Main.calculateEntropy("TestPassword123!!"));
+        assertEquals(148, Main.calculateEntropy("TestPassword123!@#$%^&*()"));
     }
 
     @org.junit.jupiter.api.Test
     void evaluateEntropy() {
+        assertEquals("Very Weak", Main.evaluateEntropy(0.0));
+        assertEquals("Very Weak", Main.evaluateEntropy(64.0));
+        assertEquals("Weak", Main.evaluateEntropy(65.0));
+        assertEquals("Weak", Main.evaluateEntropy(80));
+        assertEquals("Moderate", Main.evaluateEntropy(81.0));
+        assertEquals("Moderate", Main.evaluateEntropy(112.0));
+        assertEquals("Strong", Main.evaluateEntropy(113.0));
+        assertEquals("Strong", Main.evaluateEntropy(128.0));
+        assertEquals("Very Strong", Main.evaluateEntropy(129.0));
+        assertEquals("Very Strong", Main.evaluateEntropy(1280.0));
     }
+
 }
